d0a3912a48e6d9a7f582ec0213c85276669d1442
Ben <btmolloy2@gmail.com>
2024-12-02 19:08:00 -0500

    More File org

--- HW8/Last-First-HW8/results/code/Ver1/lib/firebase_options.dart
+++ HW8/Last-First-HW8/results/code/Ver1/lib/firebase_options.dart
@@ -3,12 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return const FirebaseOptions(
-        apiKey: "*************",
+        apiKey: "*********",
         authDomain: "ase-456-e6305.firebaseapp.com",
         projectId: "ase-456-e6305",
         storageBucket: "ase-456-e6305.firebasestorage.app",
-        messagingSenderId: "************",
-        appId: "**************",
+        messagingSenderId: "******",
+        appId: "*****",
     );
   }
 }
---
+++ HW8/Last-First-HW8/results/code/Ver2/lib/models/time_entry.dart
@@ -0,0 +1,39 @@
+class TimeEntry {
+  final String id;
+  final String date;
+  final String from;
+  final String to;
+  final String task;
+  final String tag;
+
+  TimeEntry({
+    required this.id,
+    required this.date,
+    required this.from,
+    required this.to,
+    required this.task,
+    required this.tag,
+  });
+
+
+  factory TimeEntry.fromMap(Map<String, dynamic> data, String id) {
+    return TimeEntry(
+      id: id,
+      date: data['date'] as String,
+      from: data['from'] as String,
+      to: data['to'] as String,
+      task: data['task'] as String,
+      tag: data['tag'] as String,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'date': date,
+      'from': from,
+      'to': to,
+      'task': task,
+      'tag': tag,
+    };
+  }
+}
